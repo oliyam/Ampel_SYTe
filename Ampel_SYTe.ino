a00bd58a792a44ee4ba1fd54934b01b3859fdb0b
jyameogo <jyameogo@student.tgm.ac.at>
2020-11-30 08:47:04 +0100

    Update Ampel_SYTe.ino

--- Ampel_SYTe.ino
+++ Ampel_SYTe.ino
@@ -1,18 +1,20 @@
 const int size_=3;
 int led_pins[]={2,3,4},pin_switch=5;
 bool led_state[size_]={0,0,0};
+int now_blink=0,now_cycle=0;
+unsigned long previous=0,current=millis();
 
 //ampelzyklus
 bool cycle_[][size_]={
     {0,0,1},//grün
-    {0,0,0},{0,0,1},{0,0,0},{0,0,1},{0,0,0},{0,0,1},{0,0,0},{0,0,1},//grün blinken
+    {0,0,0},{0,0,1},{0,0,0},{0,0,1},{0,0,0},{0,0,1},{0,0,0},{0,0,1},{0,0,0},//grün blinken
     {0,1,0},//gelb
     {1,0,0},//rot
     {1,1,0}//rotgelb
 };
 long cycle_interval[]={
     4500,//grün dauer
-    500,500,500,500,500,500,500,500,//grün blinken dauer
+    500,500,500,500,500,500,500,500,500,//grün blinken dauer
     2500,//gelb dauer
     5000,//rot dauer
     2500//rotgelb dauer
@@ -27,39 +29,29 @@ long blink_interval[]={
     500//gelb aus dauer
 };
 
-void setup()
-{
-  Serial.begin(9600);
+void setup(){
   for(int i=0;i<size_;i++)
     pinMode(led_pins[i], OUTPUT);
-  pinMode(pin_switch, INPUT);
+  pinMode(pin_switch, INPUT_PULLUP);
 }
 
-int now_blink=0,now_cycle=0;
-unsigned long previous=0,current=millis();
-
-void update(bool cycle[][size_], long interval[], bool output[], int& now, int cycle_length){
+void update(bool cycle[][size_], long interval[], int& now, int cycle_length){
+  for(int i=0;i<size_;i++)
+    digitalWrite(led_pins[i],cycle[now][i]);
   if(current-previous>=interval[now]){
     previous=current;
     now=(now+1)%cycle_length;
-    for(int i=0;i<size_;i++)
-      output[i]=cycle[now][i];
   }
 }
 
-void loop()
-{ 
-  current=millis();
-  
+void loop(){ 
   if(digitalRead(pin_switch)){
-    update(cycle_,cycle_interval,led_state,now_cycle,sizeof(cycle_)/size_);
-    now_blink=1;
+    update(cycle_,cycle_interval,now_cycle,sizeof(cycle_)/size_);
+    now_blink=0;
   }
   else{
-    update(blink_,blink_interval,led_state,now_blink,sizeof(blink_)/size_);
-    now_cycle=10;
-  }
-  //write to pins
-  for(int i=0;i<size_;i++)
-    digitalWrite(led_pins[i],led_state[i]);
+    update(blink_,blink_interval,now_blink,sizeof(blink_)/size_);
+    now_cycle=0;
+  } 
+  current=millis();
 }
